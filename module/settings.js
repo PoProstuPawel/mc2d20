d26b5263d177957f870c6d615f1f60a14b89a28f
PoProstuPawel <109244108+PoProstuPawel@users.noreply.github.com>
2022-08-17 09:40:51 +0200

    Update settings.js

--- module/settings.js
+++ module/settings.js
@@ -1,26 +1,26 @@
 export function registerSettings() {
-    game.settings.register('ac2d20', 'partyMomentum', {
+    game.settings.register('mc2d20', 'partyMomentum', {
         name: 'Party Momentum',
         scope: 'world',
         config: false,
         default: 0,
         type: Number,
     });
-    game.settings.register('ac2d20', 'gmMomentum', {
+    game.settings.register('mc2d20', 'gmMomentum', {
         name: 'GM Momentum',
         scope: 'world',
         config: false,
         default: 0,
         type: Number,
     });
-    game.settings.register('ac2d20', 'maxMomentum', {
+    game.settings.register('mc2d20', 'maxMomentum', {
         name: 'Max Momentum',
         scope: 'world',
         config: false,
         default: 6,
         type: Number,
     });
-    game.settings.register('ac2d20', 'gmMomentumShowToPlayers', {
+    game.settings.register('mc2d20', 'gmMomentumShowToPlayers', {
         name: 'Show GM Momentum To Players',
         hint: "Shows the GM momentum window to everyone. Requires refresh on players side.",
         scope: 'world',
@@ -28,7 +28,7 @@ export function registerSettings() {
         default: false,
         type: Boolean,
     });
-    game.settings.register('ac2d20', 'maxAppShowToPlayers', {
+    game.settings.register('mc2d20', 'maxAppShowToPlayers', {
         name: 'Players Can Setup Max App',
         hint: "Allows players to settup the Party's MAX AP. Requires refresh on players side.",
         scope: 'world',
@@ -36,11 +36,11 @@ export function registerSettings() {
         default: false,
         type: Boolean,
     });
-    game.settings.register('ac2d20', 'compendium-skills', {
+    game.settings.register('mc2d20', 'compendium-skills', {
         name: 'Skills Compendium',
         scope: 'world',
         config: true,
-        default: "ac2d20.skills",
+        default: "mc2d20.skills",
         type: String,
     });
 }
